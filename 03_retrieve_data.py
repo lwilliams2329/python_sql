270e9a67e359a1640ee9df37ade8eab0715f23c7
Lewis Williams <lwilliams2329@gmail.com>
2020-08-02 10:45:35 -0400

    Done

---
+++ 03_retrieve_data.py
@@ -0,0 +1,11 @@
+#Select statement
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    #use a for loop to iterate through database, printing the results line by line
+
+    for row in c.execute("SELECT firstname, lastname from employees"):
+        print(row)
